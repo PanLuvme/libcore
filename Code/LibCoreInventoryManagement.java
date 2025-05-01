d977a856d45801f4ae2320106b862bb5ca7c5102
ttran569 <124315530+ttran569@users.noreply.github.com>
2025-05-01 09:57:21 -0700

    Update LibCoreInventoryManagement.java
    Still a work in progress, but I made a few changes

--- Code/LibCoreInventoryManagement.java
+++ Code/LibCoreInventoryManagement.java
@@ -1,7 +1,32 @@
 import java.util.*;
 
-List<Resource> resourceList = new ArrayList<>();
+public class LibCoreInventoryManagement {
+	private List<Resource> resourceList = new ArrayList<Resource>();
 
-void addResource(Resource resource) {};
-void editResouce(Resource resource) {};
-void removeResouce(Resource resource) {};
+	public void addResource(String title, String author, String category) {
+		for (int i = 0; i < resourceList.size(); i++) {
+			Resource addedResource = new Resource(title, author, category);
+			resourceList.add(addedResource);
+		}
+	}
+	
+	public void editResource(String resourceTitle, String ) {
+		for (int i = 0; i < resourceList.size(); i++) {
+			Resource inlist = resourceList.getTitle(resourceTitle);
+			if (inlist.equalsIgnoreCase(resource)) {
+				resourceList.set
+			}
+		}
+	}
+	
+	public void removeResource(Resource resource) {
+		String stringAsResource = String.valueOf(resource);
+		boolean removed = resourceList.remove(resource);
+		if (removed == true) {
+			return;
+		}
+		else {
+			System.out.println("Not Removed");
+		}
+	}
+}
